e7a3f9a86a7eee9ddbfd8994a40066d5e9559679
OSAMA SEID <119799518+Osamaseid@users.noreply.github.com>
2024-03-20 10:02:10 +0300

    javascript

---
+++ Array/index.js
@@ -0,0 +1,11 @@
+// Example of manipulating arrays
+var fruits = ["apple", "banana", "orange"];
+fruits.push("habhab");
+fruits.pop();
+fruits.shift();
+fruits.unshift("temer");
+fruits.splice(1, 1);
+// Example of array iteration
+fruits.forEach(function (fruit) {
+    console.log(fruit);
+});
